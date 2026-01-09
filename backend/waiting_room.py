5de3453aa9ed72de62d3a44a6fd397256afaba72
Sameeratmaj <sameer.connect1103@gmail.com>
2026-01-09 19:18:50 +0530

    reduction in inactive timeout

--- backend/waiting_room.py
+++ backend/waiting_room.py
@@ -30,7 +30,7 @@ redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST"),
     port=os.getenv("REDIS_PORT"),
     password=os.getenv("REDIS_PASSWORD"),
-    decode_responses=True
+    decode_responses=True,
     max_connections=20
 )
 
